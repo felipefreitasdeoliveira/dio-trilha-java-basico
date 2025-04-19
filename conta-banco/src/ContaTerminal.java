00c30cbc9b6f98ffe534ec8125d908f814c3aa09
Felipe Freitas <oliveife@gmail.com>
2025-04-19 01:14:39 -0300

    Entrega do desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Cria o scanner para receber os dados do teclado do usuario.
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite sua Conta Bancaria: ");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Digite sua Agencia: ");
+        String agencia = scanner.next();
+        scanner.nextLine();
+
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.println("Digite o Saldo de sua conta: ");
+        double saldo = scanner.nextDouble();
+        
+        // "Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque".
+        System.out.printf("%n Olá, %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo é R$ %.2f e já está disponível para saque.\n", nomeCliente, agencia, numero, saldo);
+
     }
 }
